8594cb9651678d7617fbb62e5e3b6b03069bbab2
Andrew Fisher <andrewfishersb@gmail.com>
2019-05-11 15:34:32 -0700

    Set up adapter to recyclerview

--- app/src/main/java/com/fisher/andrew/castlerock/MainActivity.java
+++ app/src/main/java/com/fisher/andrew/castlerock/MainActivity.java
@@ -1,14 +1,29 @@
 package com.fisher.andrew.castlerock;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String API_URL = "https://iatg.carsprogram.org/signs_v1/api/signs";
+    private RecyclerView mAdapterRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<TrafficSign> signList = new ArrayList<>();
+
+
+        TrafficSignAdapter adapter = new TrafficSignAdapter(signTest);
+        mAdapterRV = findViewById(R.id.trafficInfoRV);
+        mAdapterRV.setAdapter(adapter);
+        mAdapterRV.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
--- app/src/main/java/com/fisher/andrew/castlerock/TrafficSignAdapter.java
+++ app/src/main/java/com/fisher/andrew/castlerock/TrafficSignAdapter.java
@@ -1,4 +1,63 @@
 package com.fisher.andrew.castlerock;
 
-public class TrafficSignAdapter {
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TrafficSignAdapter extends RecyclerView.Adapter<TrafficSignAdapter.ViewHolder> {
+
+    private List<TrafficSign> mSigns;
+
+    public TrafficSignAdapter(List<TrafficSign> signs){
+        mSigns = signs;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_traffic_sign,parent,false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Log.d("Logged","Test" );
+
+        viewHolder.mSignName.setText(mSigns.get(i).getName());
+      //todo if true set color to something
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSigns.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView mSignName;
+        private ConstraintLayout mCurrentRow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mSignName = itemView.findViewById(R.id.signNameTv);
+            mCurrentRow = itemView.findViewById(R.id.trafficItemCl);
+
+            mCurrentRow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            });
+        }
+    }
 }
